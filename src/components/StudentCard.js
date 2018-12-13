9e5894559be67f97987b17c4c4fa8defae7c3fd1
Lufii <lullabyforinsomnia@gmail.com>
2018-12-13 08:33:52 +0200

    That was fast, delete seems to work

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import deletee from '../images/delete.svg'
+const axios = require('axios');
 
 class StudentCard extends Component {
+
+  handleDeleteButton = () =>{
+    axios({
+      method:'POST',
+      url:'http://localhost:72/student/delete',
+      data:{
+        name: this.props.name,
+        lastname: this.props.lastname,
+        birthdate: this.props.birthdate,
+        hobbies: this.props.hobbies,
+      },
+    }).then(function (response){
+      console.log(response);
+      if(response.status === 200){
+      return response;
+    }}).catch(function (error){
+      console.log(error);
+    })
+  }
+
   render() {
     return (
       <div className="addStudent">
@@ -8,6 +30,7 @@ class StudentCard extends Component {
       <p> Last Name: {this.props.lastname} </p>
       <p> Birth Date: {this.props.birthdate} </p>
       <p> Hobbies: {this.props.hobbies} </p>
+      <img src={deletee} className='app-delete' alt='Delete student' onClick={this.handleDeleteButton} />
       </div>
     );
   }
